1ebe7f2e5e3d7177f042ed12b4e2fc74e71f941a
az0mb13 <qwertyuiop.dixit3@gmail.com>
2020-05-03 01:19:11 +0530

    Deploying

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -100,7 +101,7 @@ app.get('*', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
